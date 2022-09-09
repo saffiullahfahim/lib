e1739778d761eafc791f434aef9b3537b1c48399
saffiullahfahim <fahim14@student.sust.edu>
2022-09-09 15:20:43 +0600

    Object fixed

--- custom-password.js
+++ custom-password.js
@@ -66,7 +66,7 @@ const CustomPass = {
           result += "•";
         }
         e.target.value = result;
-        d.setCaretPosition(e.target, start);
+        CustomPass.setCaretPosition(e.target, start);
         _password.status = false;
         //console.log($password);
         return;
